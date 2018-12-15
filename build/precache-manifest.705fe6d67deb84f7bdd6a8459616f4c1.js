97efd8724c5d2ca1a5e936e98a8a520345db73cf
Tichaona <tfarirayi1@gmail.com>
2018-12-15 16:59:24 +0000

    progress update

---
+++ build/precache-manifest.705fe6d67deb84f7bdd6a8459616f4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed9a3f2cf2b75ec3f06eb3f774e71d7b",
+    "url": "/static/media/ZillaSlab-Regular.ed9a3f2c.ttf"
+  },
+  {
+    "revision": "471b4108ccb02d683c4fe25882bc9180",
+    "url": "/static/media/OverpassMono-Light.471b4108.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84cbc253be1b157d9688",
+    "url": "/static/js/main.84cbc253.chunk.js"
+  },
+  {
+    "revision": "365a2b7354443adf4d93",
+    "url": "/static/js/1.365a2b73.chunk.js"
+  },
+  {
+    "revision": "84cbc253be1b157d9688",
+    "url": "/static/css/main.0bc82bb0.chunk.css"
+  },
+  {
+    "revision": "2ab7945517b072fac3b2dbd799c21bac",
+    "url": "/index.html"
+  }
+];
